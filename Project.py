cb547655232e434eb6efdf4f7265b65cf68255a1
Almely Ramos <almely2009@hotmail.com>
2018-11-01 13:58:20 -0400

    Commits

---
+++ Project.py
@@ -0,0 +1,26 @@
+#This read the music directory and prints it on the screen.
+import os,sys
+from tkinter import *
+
+root = Tk()
+root.minsize(400,500)
+root.title("Music Manager/Player")
+
+#songlist = []
+
+#index = 0
+
+
+#path = 'C:\\Users\\new\\Music\\My Songs'
+#dirs = os.listdir( path )
+#print(dirs)
+
+#import eyed3
+#audiofile = eyed3.load("C:\\Users\\new\\Music\\My Songs")
+#print (audiofile.tag.album)
+#print(audiofile.tag.title)
+#print(audiofile.tag.album_artist)
+
+
+
+root.mainloop()
